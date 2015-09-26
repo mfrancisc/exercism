a6c84f36905719cea95647a86d70c34ed15f01e9
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-09-27 01:16:27 +0200

    Completed Hamming distance

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,13 +1,51 @@
 <?php
-
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-function distance($a, $b)
+/**
+ * Calculates the hamming distance between two stings
+ *
+ * @param $firstDna string
+ * @param $secondDna string
+ * @return $distance int number of differents nucleotides 
+ */
+function distance($firstDna, $secondDna)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+  //assert same number of nucleotides
+  if(differentStrandsLength($firstDna, $secondDna)){
+    throw new InvalidArgumentException("DNA strands must be of equal length.");
+  }
+
+  return countHammingDistance($firstDna, $secondDna);
+}
+
+/**
+ * Returns true if strands length are different , false if same length
+ *
+ * @param $firstDna string
+ * @param $secondDna string
+ * @return $different bool  true if different dna length/false if same dna length
+ */
+function differentStrandsLength($firstDna, $secondDna)
+{
+  return strlen($firstDna) !== strlen($secondDna);
+}
+
+/**
+ * Counts Hamming distance
+ *
+ * @param $firstDna string
+ * @param $secondDna string
+ * @return $hammingDistance int 
+ */
+function countHammingDistance($firstDna, $secondDna)
+{
+  $hammingDistance = 0;
+
+  for ($i=0; $i < strlen($firstDna); $i++){
+
+    if($firstDna[$i] !== $secondDna[$i]){
+      $hammingDistance++;
+    }
+
+  }
+
+  return $hammingDistance;
 }
